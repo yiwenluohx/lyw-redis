38ab28951eb0ffd0cab9d3aaec621522ead0bb1e
luohx <Lhongxiao@lcchain.com>
2021-06-18 17:17:25 +0800

    jedis-boot新增分布式锁接口

--- jedis-boot/src/main/java/com/study/redis/controller/RedisTestController.java
+++ jedis-boot/src/main/java/com/study/redis/controller/RedisTestController.java
@@ -2,6 +2,7 @@ package com.study.redis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,4 +25,34 @@ public class RedisTestController {
         String name = (String) redisTemplate.opsForValue().get("name");
         return name;
     }
+
+    @GetMapping("testLock")
+    public void testLock() {
+        //1 获取锁，setne
+        Boolean lock = redisTemplate.opsForValue().setIfAbsent("lock", "111");
+        //2 获取锁成功，查询num的值
+        if (lock) {
+            Object value = redisTemplate.opsForValue().get("num");
+            //2.1 判断num为空return
+            if (StringUtils.isEmpty(value)) {
+                return;
+            }
+            //2.2 有值就转成int
+            int num = Integer.parseInt(value + "");
+            //2.3 把redis的num加1
+            redisTemplate.opsForValue().set("num", ++num);
+            //2.4 释放锁，del
+            redisTemplate.delete("lock");
+        } else {
+            //3 获取锁失败、每隔0.1秒再获取
+            try {
+                Thread.sleep(100);
+                testLock();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
 }
